5e3cfcf093b6acdea0412cd7940ba70516d5352d
Lahari465 <laharisabbathi@gmail.com>
2026-08-13 23:43:09 +0530

    faulty_calc

---
+++ Faulty_Calculator/index.js
@@ -0,0 +1,25 @@
+
+var a =  Number(prompt("Enter digit"))
+var b = Number(prompt("Enter another digit"))
+
+var op =prompt("enter the operation");
+ if(op == '+')
+ {
+  console.log(a-b);
+ }
+ else if(op=='*')
+ {
+  console.log(a+b);
+ }
+ else if(op== '-')
+ {
+  console.log(a/b);
+ }
+ else if(op == '/')
+{
+  console.log(a**b);
+}
+else
+{
+  console.log("goodnight");
+}
