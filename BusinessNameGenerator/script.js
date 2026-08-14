57659addb11191ff0ec846820740a4e90688d999
Lahari <laharisabbathi@gmail.com>
2026-08-14 14:43:11 +0530

    Add files via upload

---
+++ BusinessNameGenerator/script.js
@@ -0,0 +1,23 @@
+let obj1 ={
+  1:"Crazy",
+  2:"Amazing",
+  3:"Fire"
+}
+
+let obj2={
+  1:"Engine",
+  2:"Foods",
+  3:"Garments"
+}
+
+let obj3={
+  1:"Bros",
+  2:"Limitied",
+  3:"Hub"
+}
+
+let a = Math.floor(Math.random()*3)+1
+let b = Math.floor(Math.random()*3)+1
+let c = Math.floor(Math.random()*3)+1
+
+console.log(obj1[a] +" " + obj2[b] + " " + obj3[c])
